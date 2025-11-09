f8d1b6a53b468b0a6473cfa8c49f14548addda7a
Harsh-chhatre <129000375+Harsh-chhatre@users.noreply.github.com>
2025-11-09 16:24:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_UqlpZhQ9N', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3sroi575b4p6fvenjqi5pljgk5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_5sfkf0x7l', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3p9kf7eu04najq8frr5tuoq131', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
